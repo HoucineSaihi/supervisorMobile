9f32a0228c46ad0dff1a0c7d4c0808f464270749
HoucineSaihi <houcinesaihi6@gmail.com>
2024-10-27 18:32:14 +0100

    good

--- lib/utils/constants/apiURL.dart
+++ lib/utils/constants/apiURL.dart
@@ -1,6 +1,5 @@
 class Apiurl{
   //static const String URL = "http://192.168.2.59:8080";
-  //static const String URL = "http://localhost:7000";
-  static const String URL = "http://192.168.168.200:7023";
-
+  static const String URL = "http://localhost:7000";
+  //static const String URL = "http://192.168.168.200:7023";
 }
